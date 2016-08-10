d729d67da8004e1bc65cb63bbf8799547cc10b36
swaidyarathna <swaidyarathna@isa.ae>
2016-08-10 10:24:09 +0530

    added docker file

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,24 +1,24 @@
 package hello;
 
-import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
